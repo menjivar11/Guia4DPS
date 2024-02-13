0e8a8ef27e87dce5669538ab8ce7655418cb3aab
menjivar11 <124649270+menjivar11@users.noreply.github.com>
2024-02-13 15:02:17 -0600

    Add files via upload

---
+++ cart-react/src/app/components/data.js
@@ -0,0 +1,86 @@
+export const data = [
+  {
+    id: 1,
+    title: "Cien años de soledad",
+    price: 100,
+    urlImage: "https://images.penguinrandomhouse.com/cover/9780525562443",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "El señor de los anillos(Trilogía)",
+    price: 190,
+    urlImage:
+      "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Cuentos de Barro",
+    price: 30,
+    urlImage:
+      "https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Tierra de Infancia",
+    price: 30,
+    urlImage:
+      "https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Harry Potter Pack",
+    price: 390,
+    urlImage:
+      "https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113",
+    quantity: 1,
+  },
+
+  {
+    id: 6,
+    title: "El Gato Negro",
+    price: 20,
+    urlImage:
+      "https://www.anayainfantilyjuvenil.com/images/libros/grande/9788466705660-el-gato-negro-tus-libros-seleccion.jpg",
+    quantity: 1,
+  },
+
+  {
+    id: 7,
+    title: "La Odisea",
+    price: 15,
+    urlImage:
+      "https://www.loqueleo.com/do/uploads/2021/08/resized/360_portada-la-odisea.jpg",
+    quantity: 1,
+  },
+
+  {
+    id: 8,
+    title: "Maze Runner Series",
+    price: 55,
+    urlImage:
+      "https://ststephensbooks.com/cdn/shop/products/age-9-14-the-maze-runner-series-5-books-collection-set-by-james-dashner-young-adult-paperback-1.jpg?v=1629511821&width=1800",
+    quantity: 1,
+  },
+
+  {
+    id: 9,
+    title: "The Hunger Games Series",
+    price: 65,
+    urlImage:
+      "https://images.booksense.com/images/657/042/9781339042657.jpg",
+    quantity: 1,
+  },
+
+  {
+    id: 10,
+    title: "Song of Ice and Fire Series",
+    price: 70,
+    urlImage:
+      "https://m.media-amazon.com/images/I/71nK5FjjYVL._SL1110_.jpg",
+    quantity: 1,
+  },
+];
